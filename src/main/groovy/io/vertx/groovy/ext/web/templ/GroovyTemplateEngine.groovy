83a9ac566b6b8e2f142dfff613afaf141d89a89c
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-06-04 00:09:40 +0200

    first shot

--- src/main/groovy/io/vertx/groovy/ext/web/templ/GroovyTemplateEngine.groovy
+++ src/main/groovy/io/vertx/groovy/ext/web/templ/GroovyTemplateEngine.groovy
@@ -3,24 +3,27 @@ package io.vertx.groovy.ext.web.templ;
 import groovy.text.GStringTemplateEngine
 import io.vertx.groovy.ext.web.templ.impl.GStringTemplateEngineImpl
 
-trait GroovyTemplateEngine extends TemplateEngine {
+class GroovyTemplateEngine {
 
-	private static Map<Class<? extends groovy.text.TemplateEngine>, Class<? extends GroovyTemplateEngine>> impls;
-	static {
+    public static String DEFAULT_TEMPLATE_EXTENSION = "groovy"
+    public static int DEFAULT_MAX_CACHE_SIZE = 10000
+
+    private static Map<Class<? extends groovy.text.TemplateEngine>, Class<? extends TemplateEngine>> impls;
+    static {
 		impls[GStringTemplateEngine.class] = GStringTemplateEngineImpl.class
 	}
 
 	static TemplateEngine create(groovy.text.TemplateEngine engine) {
-		Class<? extends GroovyTemplateEngine> impl = impls[engine.getClass()]
-		if (!impl) {
+        Class<? extends TemplateEngine> impl = impls[engine.getClass() as Class<? extends groovy.text.TemplateEngine>]
+        if (!impl) {
 			return null
 		}
 		impl.newInstance(engine)
 	}
 
 	static TemplateEngine create(Class<? extends groovy.text.TemplateEngine> clazz) {
-		Class<? extends GroovyTemplateEngine> impl = impls[clazz]
-		if (!impl) {
+        Class<? extends TemplateEngine> impl = impls[clazz]
+        if (!impl) {
 			return null
 		}
 		impl.newInstance()
--- src/main/groovy/io/vertx/groovy/ext/web/templ/impl/GStringTemplateEngineImpl.groovy
+++ src/main/groovy/io/vertx/groovy/ext/web/templ/impl/GStringTemplateEngineImpl.groovy
@@ -4,23 +4,54 @@ import groovy.text.GStringTemplateEngine
 import groovy.text.Template
 import groovy.transform.TypeChecked
 import io.vertx.core.AsyncResult
+import io.vertx.core.Future
 import io.vertx.core.Handler
+import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
 import io.vertx.core.impl.Utils
+import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.templ.impl.CachingTemplateEngine
-import io.vertx.groovy.core.buffer.Buffer
-import io.vertx.groovy.ext.web.RoutingContext
 import io.vertx.groovy.ext.web.templ.GroovyTemplateEngine
+import io.vertx.groovy.ext.web.templ.TemplateEngine
 
 @TypeChecked
-class GStringTemplateEngineImpl extends CachingTemplateEngine<Template> implements GroovyTemplateEngine {
+class GStringTemplateEngineImpl extends CachingTemplateEngine<Template>, TemplateEngine {
 
 	GStringTemplateEngine engine
 
 	public GStringTemplateEngineImpl() {
-		this.engine = new GStringTemplateEngine(Utils.getClassLoader())
-	}
+        super(GroovyTemplateEngine.DEFAULT_TEMPLATE_EXTENSION, GroovyTemplateEngine.DEFAULT_MAX_CACHE_SIZE)
+        this.engine = new GStringTemplateEngine(Utils.getClassLoader())
+    }
 
-	public void render(RoutingContext context, String templateFileName, Handler<AsyncResult<Buffer>> handler) {
-		
-	}
+    @Override
+    void render(RoutingContext context, String templateFileName, Handler<AsyncResult<Buffer>> handler) {
+        Template template = cache.get(templateFileName)
+        if (template) {
+            renderTemplate(context, template, handler)
+        } else {
+            Vertx vertx = context.vertx()
+            String location = adjustLocation(templateFileName)
+            vertx.fileSystem().readFile(location, { result ->
+                if (result.failed()) {
+                    context.fail(result.cause())
+                } else {
+                    Buffer buff = result.result()
+                    template = engine.createTemplate(buff.toString("UTF-8"))
+                    cache.put(templateFileName, template)
+                    renderTemplate(context, template, handler)
+                }
+            })
+        }
+    }
+
+    private
+    static void renderTemplate(RoutingContext context, Template template, Handler<AsyncResult<Buffer>> handler) {
+        try {
+            String rendered = template.make(context.data())
+            handler.handle(Future.succeededFuture(Buffer.buffer(rendered)))
+        } catch (all) {
+            handler.handle(Future.failedFuture(all))
+        }
+    }
 }
